f884a6af16ae263d80052d483f4b37c5ca3921b1
Rohith Muthusamy <rohithmuthusamy98@gmail.com>
2023-06-01 13:41:01 +0530

    hi,working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +6,7 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  _location: any;
-  constructor(private platform: Platform) {
-    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
-      console.log('Back press handler!');
-      if (this._location.isCurrentPathEqualTo('/home')) {
 
-        // Show Exit Alert!
-        console.log('Show Exit Alert!');
-        this.showExitConfirm();
-        processNextHandler();
-      } else {
-
-        // Navigate to back page
-        console.log('Navigate to back page');
-        this._location.back();
-
-      }
-
-    });
-  }  showExitConfirm() {
-    throw new Error('Method not implemented.');
+  constructor(){}
   }
-}
+
